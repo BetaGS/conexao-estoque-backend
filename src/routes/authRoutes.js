9de7dc3dcec1d44ea1c5e91183295e36e11f35ea
BetaGS <gssmvilar@gmail.com>
2026-09-02 22:51:01 -0300

    correção de delete acount 1.1

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import { register, login } from '../controllers/authController.js';
+// Adicione deleteAccount aqui na importação:
+import { register, login, deleteAccount } from '../controllers/authController.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = Router();
